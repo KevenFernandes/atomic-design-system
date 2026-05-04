9c23b0278c7351da69f760cd4e811c5f4939b8d5
Keven Fernandes <kevenpyw@gmail.com>
2026-05-04 15:28:06 -0300

    feat: visualization of the new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { StarIcon } from "lucide-react";
+import { Alert } from "../components/Alert";
+import { Badge } from "../components/Badge";
 import { Button } from "../components/Button";
 import { InputText } from "../components/InputText";
 
@@ -70,6 +73,114 @@ export default function Home() {
               labelText="Label Text"
               helperText="Helper Text"
               placeholder="Placeholder"
+            />
+          </div>
+          <h2 className="my-4">Badge e Chip</h2>
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2">
+              <Badge
+                varirant="default"
+                mode="solid"
+                size="medium"
+                buttonText="Badge"
+              />
+              <Badge varirant="default" mode="solid" size="medium" />
+
+              <Badge
+                varirant="default"
+                mode="outline"
+                size="medium"
+                buttonText="Badge"
+              />
+              <Badge varirant="default" mode="outline" size="medium" />
+            </div>
+
+            <div className="flex gap-2">
+              <Badge
+                varirant="success"
+                mode="solid"
+                size="small"
+                buttonText="Badge"
+              />
+              <Badge varirant="success" mode="solid" size="small" />
+
+              <Badge
+                varirant="success"
+                mode="outline"
+                size="small"
+                buttonText="Badge"
+              />
+              <Badge varirant="success" mode="outline" size="small" />
+            </div>
+
+            <div className="flex gap-2">
+              <Badge
+                varirant="info"
+                mode="solid"
+                size="tiny"
+                buttonText="Badge"
+              />
+              <Badge varirant="info" mode="solid" size="tiny" />
+
+              <Badge
+                varirant="info"
+                mode="outline"
+                size="tiny"
+                buttonText="Badge"
+              />
+              <Badge varirant="info" mode="outline" size="tiny" />
+            </div>
+
+            <div className="flex gap-2">
+              <Badge
+                varirant="warning"
+                mode="solid"
+                size="tiny"
+                buttonText="Badge"
+              />
+              <Badge varirant="warning" mode="solid" size="tiny" />
+
+              <Badge
+                varirant="warning"
+                mode="outline"
+                size="tiny"
+                buttonText="Badge"
+              />
+              <Badge varirant="warning" mode="outline" size="tiny" />
+            </div>
+          </div>
+
+          <div className="mt-8 flex flex-col gap-4">
+            <Alert
+              variant="default"
+              title="Title"
+              description="Get immediate alerts and a notification badge."
+              icon={StarIcon}
+            />
+
+            <Alert
+              variant="default"
+              mode="outline"
+              title="Title"
+              description="Get immediate alerts and a notification badge."
+              icon={StarIcon}
+            />
+
+            {/* success */}
+
+            <Alert
+              variant="success"
+              title="Title"
+              description="Get immediate alerts and a notification badge."
+              icon={StarIcon}
+            />
+
+            <Alert
+              variant="success"
+              mode="outline"
+              title="Title"
+              description="Get immediate alerts and a notification badge."
+              icon={StarIcon}
             />
           </div>
         </div>
